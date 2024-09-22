0bae0e8ee553b3f568e2c48d1f6c36c215293fbd
Sunil <btwitsbista@gmail.com>
2024-09-22 10:20:22 +0545

    fixing eerorr

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://btwitsbista:SlUJ2OigGR7VjgS6@next-auth.kioaw.mongodb.net/next-auth?retryWrites=true&w=majority&appName=next-auth";
 
 if (!MONGODB_URI) {
   throw new Error(
